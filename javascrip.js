362412d74776a3afc73754f71216cd137489bae6
jjzapata2002 <jjzapata2002@gmail.com>
2023-08-02 10:07:24 -0500

    ActividadJs

---
+++ javascrip.js
@@ -0,0 +1,15 @@
+/*let base = prompt("ingrese la base")
+let altura =prompt("ingrese la altura")
+let area =(parseFloat(base) * parseFloat(altura) /2)
+console.log("el area del triangulo es: "+area)*/
+/*let pesos = prompt("ingrese la cantidad de pesos que posee")
+let dolar 
+let dolares =(parseFloat(pesos)/4010)
+console.log("la cantidad de dolares que posee es: " +dolares)*/
+/*let naciemiento= prompt("ingrese año de nacimiento")
+let año 
+let edad =(2023-parseFloat(naciemiento))
+console.log("su edad es: "+edad)*/
+/*let horas =prompt("ingrese las horas que estaciono")
+let pagar=(parseFloat(horas)*4500)
+console.log ("el total a pagar es: "+pagar)*/
